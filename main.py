3fa08824f1b28111049e73a1af326b8d15436711
Dyachkov <dyachkov@itsnuh.com>
2022-09-02 00:02:45 +0900

    1.add start and stop label 2.add legend to plot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+import re
 log = open("SPO2_LOG.log", 'r')
 new_data_ir = open("IR_DATA.txt", 'w')
 new_data_red = open("RED_DATA.txt", 'w')
@@ -7,9 +7,9 @@ new_data_red = open("RED_DATA.txt", 'w')
 plot_data_red = []
 plot_data_ir = []
 
+print("START PARSING, LEN ->", len(re.findall(r"[\n']+", open('SPO2_LOG.log').read())))
 while True:
     line = log.readline()
-    print("ONE_STRING-->", line)
     if line != '':
         if line[0] == '1':
             new_data_ir.write(line[8:])
@@ -21,8 +21,10 @@ while True:
         log.close()
         new_data_ir.close()
         new_data_red.close()
+        print("STOP PARSING-->", line)
         break
 
-plt.plot(plot_data_red)
-# plt.plot(plot_data_ir)
+plt.plot(plot_data_red, label="RED")
+plt.plot(plot_data_ir, label="IR")
+plt.legend()
 plt.show()
