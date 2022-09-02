cd25717305c306d0e7b215a1b2b59045914cce83
Dyachkov <dyachkov@itsnuh.com>
2022-09-02 17:10:58 +0900

    1.add check '\0' 2.add cnt pars data

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import matplotlib.pyplot as plt
 import re
+
+CNT_PART_DATA = 100000
+
 log = open("SPO2_LOG.log", 'r')
 new_data_ir = open("IR_DATA.txt", 'w')
 new_data_red = open("RED_DATA.txt", 'w')
 
 plot_data_red = []
 plot_data_ir = []
-
+cnt_line = 0
 print("START PARSING, LEN ->", len(re.findall(r"[\n']+", open('SPO2_LOG.log').read())))
 while True:
     line = log.readline()
+    line = line.replace('\0', "", 3)
+    cnt_line +=1
     if line != '':
         if line[0] == '1':
             new_data_ir.write(line[8:])
@@ -17,7 +22,7 @@ while True:
         if line[0] == '2':
             new_data_red.write(line[8:])
             plot_data_red.append(int(line[8:13]))
-    if not line:
+    if not line or cnt_line == CNT_PART_DATA:
         log.close()
         new_data_ir.close()
         new_data_red.close()
